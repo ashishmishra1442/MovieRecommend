a4aee47b6a0e9ae0e7b3f7087e177bddbd8ff2c5
Ashish Kumar Mishra <115819983+ashishmishra1442@users.noreply.github.com>
2024-06-01 14:20:57 +0530

    Update context.js

--- src/context.js
+++ src/context.js
@@ -2,11 +2,6 @@ import React, { useContext, useState } from "react";
 import useFetch from "./useFetch";
 
 const AppContext = React.createContext();
-
-/* plz subsribe to thapa technical channel 
-          https://www.youtube.com/thapatechnical
-         */
-
 // we are getting the children and that is app component in our case
 const AppProvider = ({ children }) => {
   const [query, setQuery] = useState("hacker");
